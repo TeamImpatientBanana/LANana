5132652ec805d93e198503be35c52d597a0d0d8a
Andrew Jarvis <ajarvis7771@gmail.com>
2015-09-26 17:12:40 -0400

    Added token verification

--- app.js
+++ app.js
@@ -5,6 +5,8 @@
 
 var express = require('express');
 var routes = require('./routes');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var app = module.exports = express.createServer();
 
@@ -27,9 +29,29 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
-// Routes
+// Express messages
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
+// Express session
+app.use(session({
+  genid: function(req) {
+    return genuuid() // use UUIDs for session IDs
+  },
+  secret: 'brosis'
+}));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Routes
 app.get('/', routes.index);
+app.get('/join', routes.getJoin);
+app.post('/join', routes.postJoin);
+
 
 app.listen(process.env.PORT || 5000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
+// Define global variable array
+exports.tokens = [];
 
 /*
  * GET home page.
  */
-
 exports.index = function(req, res){
+    exports.tokens.push("9999");
     res.render('index', { title: 'Express' });
+};
 
+/*
+ * GET join game page.
+ */
+exports.getJoin = function(req, res) {
+    res.render('join', {
+        title: 'Join Game',
+        token: exports.tokens[0]
+    });
+};
+
+/*
+ * POST join game page.
+ */
+exports.postJoin = function(req, res) {
+    // set a session on valid auth token
+    var token = req.body.token;
+    if (exports.tokens.indexOf(token) > -1) {
+        res.send(token);
+    }
+    else {
+        req.flash("error", "Invalid game token");
+    }
 };
